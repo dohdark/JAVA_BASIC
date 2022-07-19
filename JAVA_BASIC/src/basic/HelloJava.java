284bee5fb51242e949346072149237c4b033152e
dohdark <109597157+dohdark@users.noreply.github.com>
2022-07-19 22:02:14 +0900

    Hello JAVA

---
+++ JAVA_BASIC/src/basic/HelloJava.java
@@ -0,0 +1,11 @@
+package basic;
+
+//Project -> package -> Class
+//쇼핑몰	-> 회원		-> 회원가입, 수정, 삭제, 탈퇴..
+//		-> 주문		-> 주문, 취소, 수정, 검색, 목록
+//		-> 상품		-> 상품,.........
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA");	// ; 세미콜론 = 한줄 끝
+	}
+}
---
+++ JAVA_BASIC/src/basic/JavaVariable.java
@@ -0,0 +1,18 @@
+package basic;
+
+// 변수 : 하나의 값을 저장할 수 있는 메모리 공간
+
+// 자료형(데이터 타입)
+//	1.기본자료형 (8가지)
+//	 -short, byte, int, long
+//	 -float, double
+//	 -char
+//	 -boolean
+//
+//	2.객체자료형 (기본자료형 제외한 모든 자료형은 객체자료형이다)
+//	 -Class
+//	 -Array
+//	 -String
+public class JavaVariable {
+
+}
